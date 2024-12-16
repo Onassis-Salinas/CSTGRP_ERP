026484ecef3641daeff10150948b60e9479fe95b
onassis <onassis.dev@gmail.com>
2024-12-16 12:38:19 -0800

    fixed screen height

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,8 +1,6 @@
-<script>
-</script>
-
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<section>Bienvenido al sistema de gestion cst</section>
+<section class="flex h-full w-full items-center justify-center">
+	<div>
+		<img src="/logo.png" alt="logo" class="size-28 mb-6" />
+		<p class="text-center font-medium text-3xl">CST <br/> ERP</p>
+	</div>
+</section>
