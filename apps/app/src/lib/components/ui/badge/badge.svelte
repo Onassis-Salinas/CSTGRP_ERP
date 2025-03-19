7b57ce2cd2efdbb9f4a1e18a42fc0888766f7175
Onassis <onassis.dev@gmail.com>
2025-03-19 12:36:13 -0700

    fix(app): Correct badge colors

--- apps/app/src/lib/components/ui/badge/badge.svelte
+++ apps/app/src/lib/components/ui/badge/badge.svelte
@@ -2,15 +2,22 @@
 	import { type VariantProps, tv } from 'tailwind-variants';
 
 	export const badgeVariants = tv({
-		base: 'focus:ring-ring inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
+		base: 'focus:ring-ring outline outline-1 inline-flex select-none items-center rounded-[6px] px-1.5  transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
 		variants: {
 			variant: {
-				default: 'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent',
-				secondary:
-					'bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent',
-				destructive:
-					'bg-destructive text-destructive-foreground hover:bg-destructive/80 border-transparent',
-				outline: 'text-foreground'
+				default: 'bg-primary text-primary-foreground hover:bg-primary/80 ',
+				secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80 ',
+				gray: 'bg-gray text-gray-foreground outline-gray-foreground/20 ',
+				red: 'bg-red text-red-foreground outline-red-foreground/20 ',
+				brown: 'bg-brown text-brown-foreground outline-brown-foreground/20 ',
+				orange: 'bg-orange text-orange-foreground outline-orange-foreground/20 ',
+				lime: 'bg-lime text-lime-foreground outline-lime-foreground/20 ',
+				green: 'bg-green text-green-foreground outline-green-foreground/20 ',
+				cyan: 'bg-cyan text-cyan-foreground outline-cyan-foreground/20 ',
+				blue: 'bg-blue text-blue-foreground outline-blue-foreground/20 ',
+				purple: 'bg-purple text-purple-foreground outline-purple-foreground/20 ',
+				yellow: 'bg-yellow text-yellow-foreground outline-yellow-foreground/20 ',
+				pink: 'bg-pink text-pink-foreground outline-pink-foreground/20 '
 			}
 		},
 		defaultVariants: {
@@ -30,11 +37,11 @@
 		ref = $bindable(null),
 		href,
 		class: className,
-		variant = 'default',
+		color = 'default',
 		children,
 		...restProps
 	}: WithElementRef<HTMLAnchorAttributes> & {
-		variant?: BadgeVariant;
+		color?: BadgeVariant;
 	} = $props();
 </script>
 
@@ -42,7 +49,7 @@
 	this={href ? 'a' : 'span'}
 	bind:this={ref}
 	{href}
-	class={cn(badgeVariants({ variant }), className)}
+	class={cn(badgeVariants({ variant: color }), className)}
 	{...restProps}
 >
 	{@render children?.()}
